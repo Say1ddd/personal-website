f1ce5053e29b1a1c5b3932cae7f6693473721e7a
Sayiddd <sajidmuhammadikhlas@gmail.com>
2025-11-20 23:00:09 +0700

    Initial commit

---
+++ src/pages/about.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'AboutPage',
+})
+
+const router = useRouter()
+
+function toHome() {
+  router.push(`/`)
+}
+
+const { t } = useI18n()
+
+useHead({
+  title: () => t('button.about'),
+})
+</script>
+
+<template>
+  <div>
+    <h1>About Page</h1>
+    <button btn @click="toHome">
+      <div i-carbon-home />
+    </button>
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+layout: default
+</route>
---
+++ src/pages/index.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'IndexPage',
+})
+
+const router = useRouter()
+
+function toAbout() {
+  router.push(`/about`)
+}
+
+const { t } = useI18n()
+
+useHead({
+  title: () => t('button.home'),
+})
+</script>
+
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <button btn @click="toAbout">
+      <div i-carbon-border-full />
+    </button>
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: home
+</route>
