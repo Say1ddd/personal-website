1e07ce259a17a95d28affe00e3148f73b7a07caf
Sayiddd <sajidmuhammadikhlas@gmail.com>
2025-11-21 19:37:23 +0700

    CSS resets

--- uno.config.ts
+++ uno.config.ts
@@ -6,8 +6,8 @@ import {
   presetAttributify,
   presetIcons,
   presetTypography,
-  presetUno,
   presetWebFonts,
+  presetWind3,
   presetWind4,
   transformerDirectives,
   transformerVariantGroup,
@@ -19,7 +19,7 @@ export default defineConfig({
     ['icon-btn', 'inline-block cursor-pointer select-none opacity-75 transition duration-200 ease-in-out hover:opacity-100 hover:text-teal-600'],
   ],
   presets: [
-    presetUno(),
+    presetWind3,
     presetWind4,
     presetAttributify(),
     presetIcons({
