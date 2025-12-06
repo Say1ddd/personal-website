27829abef5080e9b72fb952f849e14af8a3babba
Sayiddd <sajidmuhammadikhlas@gmail.com>
2025-12-06 17:56:43 +0700

    Create initial route content

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -9,7 +9,7 @@ function toHome() {
   router.push(`/`)
 }
 
-const { t } = useI18n()
+const { t, tm, rt } = useI18n()
 
 useHead({
   title: () => t('button.about'),
@@ -22,6 +22,26 @@ useHead({
     <button btn @click="toHome">
       <div i-carbon-home />
     </button>
+    <h1>{{ t('about.title') }}</h1>
+    <h2>{{ t('about.subtitle') }}</h2>
+    <ul>
+      <li v-for="(section, i) in tm('about.sections')" :key="i">
+        <h1>{{ rt(section._label) }}</h1>
+        <ul v-for="(item, j) in section.items" :key="j">
+          <li>
+            <h3>{{ rt(item.title) }}</h3>
+            <template v-if="item.subtitle.length > 0">
+              <ul v-for="(value, k) in item.subtitle" :key="k">
+                {{ rt(value) }}
+              </ul>
+            </template>
+            <h4 v-else>
+              {{ rt(item.subtitle) }}
+            </h4>
+          </li>
+        </ul>
+      </li>
+    </ul>
   </div>
 </template>
 
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -17,12 +17,21 @@ useHead({
 </script>
 
 <template>
-  <div h="full" overflow="x-hidden y-scroll" snap="y mandatory">
+  <div class="relative" h="full" overflow="x-hidden y-scroll" snap="y mandatory">
     <section snap="always center" h="full">
-      <h1>home page</h1>
+      <h1>{{ t('home.section_1') }}</h1>
       <button data-test-id="about" btn @click="toAbout">
         <div i-carbon-border-full />
       </button>
+    </section>
+    <section snap="always center" h="full">
+      <div>
+        <h1>{{ t('home.section_2.title') }}</h1>
+        <h2>{{ t('home.section_2.subtitle') }}</h2>
+      </div>
+    </section>
+    <section snap="always center" h="full">
+      <h1>Let's connect!</h1>
     </section>
   </div>
 </template>
