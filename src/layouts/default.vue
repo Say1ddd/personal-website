6e0c1c09c822572ca5cf5be265a176feff055f5a
Sayiddd <sajidmuhammadikhlas@gmail.com>
2026-01-27 23:25:50 +0700

    Add 'PreviousButton' component for '/' navigation

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,15 +1,35 @@
 <script setup lang="ts">
+const route = useRoute()
+const router = useRouter()
+
+const isInRoot = computed(() => route.path === '/')
+const toRoot = () => router.push('/')
+
+const transitionClass = {
+  enterFromLeaveTo: 'opacity-0 rotate-90 translate-x-24',
+  enterActive: 'pointer-events-none transition duration-500 delay-300',
+  leaveActive: 'pointer-events-none transition delay-500',
+}
 </script>
 
 <template>
-  <main class="font-manrope">
+  <main>
     <LayoutDivider />
     <TheHeader />
 
-    <LayoutDivider class="wrapper flex-1 *:rounded-br-lg *:border-dashed">
-      <RouterView v-slot="{ Component, route }">
+    <LayoutDivider class="wrapper flex-1 relative *:rounded-br-lg *:border-dashed">
+      <Transition
+        mode="out-in"
+        :enter-from-class="transitionClass.enterFromLeaveTo"
+        :enter-active-class="transitionClass.enterActive"
+        :leave-active-class="transitionClass.leaveActive"
+        :leave-to-class="transitionClass.enterFromLeaveTo"
+      >
+        <PreviousButton v-if="!isInRoot" @click="toRoot" />
+      </Transition>
+      <RouterView v-slot="{ Component, route: r }">
         <Transition name="fade" mode="out-in">
-          <component :is="Component" :key="route.path" />
+          <component :is="Component" :key="r.path" />
         </Transition>
       </RouterView>
     </LayoutDivider>
@@ -27,21 +47,26 @@ main {
   scroll-snap-type: y mandatory;
   overflow: hidden;
   flex: auto;
-  background-color: var(--color-background);
-  background-image: url('/assets/patterns/graph-paper.svg');
+}
+
+main div.wrapper::before,
+main div.wrapper::after {
+  mask-image: url('/assets/patterns/texture.svg');
 }
 
 main div.wrapper::before {
-  background-image: url('/assets/patterns/diagonal-lines.svg');
-  filter: invert();
+  background: linear-gradient(to bottom left, transparent 25%, var(--color-foreground));
 }
 
+main div.wrapper::after {
+  background: linear-gradient(to bottom right, transparent 25%, var(--color-foreground));
+}
+
+/*
 main div.wrapper {
-  background-color: var(--color-background);
-  /*
   -webkit-box-shadow: 0 0 1rem 1rem var(--color-background);
   -moz-box-shadow: 0 0 1rem 1rem var(--color-background);
   box-shadow: 0 0 1rem 1rem var(--color-background);
-   */
 }
+*/
 </style>
