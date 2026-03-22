2e47f13b5180da60cc961e54ec4f5d486d2f2dbf
Sayiddd <sajidmuhammadikhlas@gmail.com>
2026-03-22 14:15:05 +0700

    Modify locale content

--- src/components/partials/ScrollIndicator.vue
+++ src/components/partials/ScrollIndicator.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
+const { t } = useI18n()
 const { isScrolling, isToBottom, isOnBottom } = useInjectedScroll()
-
 const isScrollingDown = computed(() => isScrolling.value || isToBottom.value || isOnBottom.value)
 </script>
 
 <template>
   <div aria-hidden="true" fixed flex="~ items-end justify-end sm:justify-center" m="y-10 sm:y-20 md:y-24 lg:y-32 x-12" transition="composite duration-300" class="events-none inset-0" :class="isScrollingDown ? 'opacity-0 animate-paused' : 'opacity-75'">
     <div class="animate-bounce">
-      <span select="none" text="sm md:base" font="medium jetbrains-mono">scroll down</span>
+      <span select="none" text="sm md:base" font="medium jetbrains-mono">
+        {{ t('scroll') }}
+      </span>
       <div m="x-auto" i="carbon-chevron-down" />
     </div>
   </div>
