10139be96b427c0e6d6c9e3aacbd967633ab76a3
Sayiddd <sajidmuhammadikhlas@gmail.com>
2026-03-22 14:19:12 +0700

    Modify home page 'hero' section
    Move modified 'hero' section into separate component 'IndexFirstSection' (additional component added: LinkCTA, HeroBackgroundTag, HeroButtonIndicator, HeroContent, HeroCTA, HeroHeading)

---
+++ src/components/partials/index/IndexFirstSection.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const visibilityTarget = useTemplateRef('sectionRef')
+const isVisible = useElementVisibility(visibilityTarget, {
+  threshold: 2 / 3,
+})
+</script>
+
+<template>
+  <section ref="sectionRef" transition="composite duration-1000 ease-out-expo" class="section-idx origin-top items-center" :class="!isVisible && `opacity-0 scale-y-50 events-none`">
+    <div flex="~ 1 col" gap="8" overflow="hidden" class="group">
+      <HeroContent :is-visible="isVisible" />
+      <HeroCTA :is-visible="isVisible" />
+      <div aria-hidden="true" grid="~ cols-12" p="x-0.5" class="opacity-50 skew-x-45">
+        <div border="t-2 dotted foreground" class="col-span-1" />
+        <div border="t-2 dotted primary" m="x-1" class="col-span-8" />
+        <div border="t-2 dotted foreground" class="col-span-3" />
+      </div>
+    </div>
+  </section>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,20 +9,7 @@ useHead({
 <template>
   <div class="relative" overflow="x-hidden y-auto" h="full" snap="y mandatory">
     <ScrollIndicator />
-    <section class="section-idx">
-      <div class="items-center justify-center" flex="~ 1" p="4">
-        <div container flex="~ col" gap="4" border="l-4 primary" class="pl-4 container">
-          <h1 class="heading-1">
-            {{ t('home.section_1') }}
-          </h1>
-          <div flex="1" class="">
-            <RouterLink to="/about" data-cy="about" p="y-2 x-3" class="text-black tracking-widest font-bold bg-primary w-fit select-none" border="~ r-4 primary">
-              <span>ABOUT ME</span>
-            </RouterLink>
-          </div>
-        </div>
-      </div>
-    </section>
+    <IndexFirstSection />
     <IndexSecondSection />
     <IndexThirdSection />
   </div>
